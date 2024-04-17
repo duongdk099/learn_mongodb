7c65f942469c20fe84695f8853e157ed82272181
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2024-04-17 11:08:02 +0200

    connect to DB MongoDB OKE

---
+++ server.js
@@ -0,0 +1,11 @@
+require ("dotenv").config();
+const app = require("./app");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
