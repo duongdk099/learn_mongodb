bb77350a08fe93af86e923d7ba06e93424c956cb
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2024-04-18 16:09:59 +0200

    make routes, CRUD user and account

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connectDB = require('./app/models/index.js');
 const router = require('./app/routes/index.js');
 
 const app = express();
+app.use(express.json()); // middle ware for parsing json
 app.use("/api", router);
 
 module.exports = app;
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,11 +1,36 @@
-const {User} = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
-    // const { email, password } = req.body;
-    // console.log(req.body, "hello");
-    res.send("Hello to sign up ");
-}
+  const { email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email: email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.login = async (req, res) => {
-    res.send("Hello to login ");
-}
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.log("Invalid email or password");
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "10h",
+    });
+    res.status(200).json({ token, user: { id: user._id, email: user.email } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
